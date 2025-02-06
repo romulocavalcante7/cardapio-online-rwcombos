eb6e3c7a40ff37b9132e0912ad460815f46014d0
Rômulo <romulogomescavalcante7@gmail.com>
2025-02-06 09:56:21 -0300

    first commit

---
+++ js/dados.js
@@ -0,0 +1,489 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio/churrasco/combo.png",
+            "name": "COMBO BIG",
+            "dsc": "2 Hamburguers + Fritas + Salgados",
+            "price": 50
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/churrasco/combo2.png",
+            "name": "COMBO FAMILY",
+            "dsc": "2 Hamburguers + Salgados + 2 Pastel",
+            "price": 45
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/churrasco/combo3.png",
+            "name": "COMBO INDIVIDUAL",
+            "dsc": "1 Hamburguer + 1 Coca + Batata",
+            "price": 26
+        },
+        
+        
+        /*
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "./img/cardapio/churrasco/mini-trinity-bbq-combo-brisket-ribs-and-links.245582f593bf64b23b57dfca2be18cfd.jpg",
+            "name": "Bludso's BBQ",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 139
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "./img/cardapio/churrasco/blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo.a95a3e632ae324f719738a2a5c1dff6e.jpg",
+            "name": "Blue Smoke",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 129
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/cardapio/churrasco/bbq-meat-lovers-special-for-10.36ca670fda4bfa783c2ea9165e068d26.jpg",
+            "name": "Kings BBQ",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 139
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "./img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
+            "name": "Ugly Drum",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 229
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "./img/cardapio/churrasco/mighty-quinns-bbq-sampler-pack.1bfe4a0665edc565756f5241bf25840e.jpg",
+            "name": "Mighty Quinn's BBQ",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 169
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "./img/cardapio/churrasco/post-oak-smoked-usda-prime-half-brisket.6f17178dde7d806670bcc73ff11762b3.jpg",
+            "name": "Southside Market & Barbeque",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 109
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 269
+        }*/
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/cardapio/burguers/CENTRO.jpg",
+            "name": "CENTRO DE SALGADOS",
+            /* teste do ingrediente*/
+            "dsc": "Combinar no WhatsApp",
+            "price": 65
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "./img/cardapio/burguers/2.png",
+            "name": "ENROLADINHO ",
+            "dsc": " ",
+            "price": 1
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "./img/cardapio/burguers/3.png",
+            "name": "BOLINHA ",
+            "dsc": "QUEIJO",
+            "price": 1.50
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/cardapio/burguers/4.png",
+            "name": "BOLINHO",
+            "dsc": "CARNE",
+            "price": 1
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/cardapio/burguers/5.png",
+            "name": "RISOLE",
+            "dsc": "PRESUNTO, QUEIJO E CARNE",
+            "price": 1
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "./img/cardapio/burguers/6.png",
+            "name": "BOMBA",
+            "dsc": "PRESUNTO E QUEIJO",
+            "price": 1
+        },
+        
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/8.png",
+            "name": "COXINHA",
+            "dsc": "FRANGO COM CATUPIRY",
+            "price": 5
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/pastel.png",
+            "name": "PASTEL",
+            "dsc": "Combinar no WhatsApp",
+            "price": 7
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/F.png",
+            "name": "FOGAZZES",
+            "dsc": "Combinar no WhatsApp",
+            "price": 6
+        },
+        
+        /*
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 109
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg",
+            "name": "Raoul's",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img": "./img/cardapio/burguers/goldbelly-burger-blend-1-lb.13a21b66edf7173a59c75c3a6d2f981b.jpg",
+            "name": "Flannery Beef",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 79
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-8.092aa049d00286fa1733d720decc782e.jpg",
+            "name": "Gott's Roadside",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 149
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
+            "name": "Gramercy Tavern",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 149
+        }*/
+    ],
+    "sobremesas": [
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/1.png",
+            "name": "FANTA-LARANJA",
+            "dsc": "LATA",
+            "price": 5
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/sobremesas/2.png",
+            "name": "GUARANÁ-ANTÁRTICA",
+            "dsc": "LATA",
+            "price": 5
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/cardapio/sobremesas/3.png",
+            "name": "COCA-COLA",
+            "dsc": "LATA",
+            "price": 5
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/cardapio/sobremesas/4.png",
+            "name": "GUARANÁ-ANTÁRTICA 2L",
+            "dsc": "2 LITROS",
+            "price": 13
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/cardapio/sobremesas/5.png",
+            "name": "GUARANÁ-ANTÁRTICA 1L",
+            "dsc": "1 LITRO",
+            "price": 8
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "./img/cardapio/sobremesas/6.png",
+            "name": "FANTA-LARANJA 2L",
+            "dsc": "2 LITROS",
+            "price": 13
+        },
+        {
+            "id": "dessert-bar-care-package",
+            "img": "./img/cardapio/sobremesas/8.png",
+            "name": "FANTA-LARANJA 2L",
+            "dsc": "1 LITRO",
+            "price": 8
+        },
+        {
+            "id": "donut-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/9.png",
+            "name": "COCA-COLCA 2L",
+            "dsc": "2 LITROS",
+            "price": 14
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+            "img": "./img/cardapio/sobremesas/10.png",
+            "name": "COCA-COLA 1L",
+            "dsc": "1 LITRO",
+            "price": 9
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/11.png",
+            "name": "COCA-COLA 1,5L",
+            "dsc": "1,5 LITROS",
+            "price": 11
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/1.png",
+            "name": "GOIABA ",
+            "dsc": "NATURAL 300ML",
+            "price": 5
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/bebidas/2.png",
+            "name": "ACEROLA",
+            "dsc": "NATURAL 300ML",
+            "price": 5
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cardapio/bebidas/3.png",
+            "name": "ABACAXI",
+            "dsc": "NATURAL 300ML",
+            "price": 7
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cardapio/bebidas/4.png",
+            "name": "MARACUJÁ",
+            "dsc": "NATURAL 300ML",
+            "price": 8
+        },
+        
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cardapio/bebidas/6.png",
+            "name": "CAJÚ",
+            "dsc": "NATURAL 300ML",
+            "price": 5
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cardapio/bebidas/7.png",
+            "name": "MANGA",
+            "dsc": "NATURAL 300ML",
+            "price": 5
+        },
+        /*
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cardapio/bebidas/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
+            "name": "The Hummus & Pita Co.",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 89
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cardapio/bebidas/old-honey-barn-mint-julep-mixer-200ml.e0b131d6d9b69963706b43fd4334ab74.jpg",
+            "name": "Old Honey Barn Mint Julep",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 25
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cardapio/bebidas/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 59.95
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cardapio/bebidas/weekender-charleston-bloody-mary-mix-bold-and-spicy.c372868c9937e407a299a22001e210e2.jpg",
+            "name": "Charleston Beverage Company",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 39.95
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/bebidas/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
+            "name": "Grady's Cold Brew",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 40
+        }*/
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/1.png",
+            "name": "ESFIRRA",
+            "dsc": "Combinar sabor no WhatsApp",
+            "price": 2
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/2.png",
+            "name": "BAURU",
+            "dsc": "",
+            "price": 2
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/4.png",
+            "name": "HAMBURGÃO",
+            "dsc": "Cheddar, carne de hambúrguer",
+            "price": 3
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/3.png",
+            "name": "PÃO RECHEADO",
+            "dsc": "Cheddar, carne de hambúrguer",
+            "price": 2
+        },
+        
+        
+    ],
+    "steaks": [
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/combo4.png",
+            "name": "COMBO M",
+            "dsc": "(TAMANHO DO COMBO)",
+            "price": 35
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/combo4.png",
+            "name": "COMBO P",
+            "dsc": "(TAMANHO DO COMBO)",
+            "price": 28
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/combo4.png",
+            "name": "COMBO G",
+            "dsc": "(TAMANHO DO COMBO)",
+            "price": 50
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/14.png",
+            "name": "MANDIOCA FRITA",
+            "dsc": "ACOMPANHAMENTO",
+            "price": 0
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/13.png",
+            "name": "CATUPIRY+BACON",
+            "dsc": "ACOMPANHAMENTO",
+            "price": 0
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/12.png",
+            "name": "CHEDDAR+BACON",
+            "dsc": "ACOMPANHAMENTO",
+            "price": 0
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/11.png",
+            "name": "NUGGETS",
+            "dsc": "ACOMPANHAMENTO",
+            "price": 0
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/10.png",
+            "name": "SALGADOS",
+            "dsc": "ACOMPANHAMENTO",
+            "price": 0
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/9.png",
+            "name": "CALABRESA ACEBOLADA",
+            "dsc": "ACOMPANHAMENTO",
+            "price": 0
+        },
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/1.png",
+            "name": "Molho de pimenta",
+            "dsc": "",
+            "price": 0
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/2..png",
+            "name": "Ketchup",
+            "dsc": "",
+            "price": 0
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/3.png",
+            "name": "Molho verde",
+            "dsc": "",
+            "price": 0
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/4.png",
+            "name": "Molho caseiro",
+            "dsc": "",
+            "price": 0
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/5.png",
+            "name": "Barbecue",
+            "dsc": "",
+            "price": 0
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/6.png",
+            "name": "Mostarda",
+            "dsc": "",
+            "price": 0
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/7.png",
+            "name": "Maionese",
+            "dsc": "",
+            "price": 0
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/8.png",
+            "name": "Molho Rosé",
+            "dsc": "",
+            "price": 0
+        },
+        
+    ]
+}
